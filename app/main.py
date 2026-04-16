bc1eedcd487832a2dbe487938248db5423001b65
Fauzan <azharfauzan472@gmail.com>
2026-04-17 01:32:48 +0700

    update feature custom link

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 #  app\main.py
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-
 from fastapi.responses import RedirectResponse
-from fastapi import HTTPException
+# from fastapi import HTTPException
 
 from app.database import engine, Base, SessionLocal
 from app import models
 from app.schemas import URLCreate
 from app.utils import generate_short_code
+
+import re
 
 app = FastAPI()
 
@@ -26,9 +27,33 @@ def get_db():
 def root():
     return {"message": "API jalan!"}
 
+
+
 @app.post("/shorten")
 def create_short_url(data: URLCreate, db: Session = Depends(get_db)):
-    short_code = generate_short_code()
+
+    if data.custom_code:
+
+        if not re.match("^[a-zA-Z0-9_-]+$", data.custom_code):
+            raise HTTPException(
+                status_code=400,
+                detail="Custom code only allows letters, numbers, - and _"
+            )
+
+        existing = db.query(models.URL).filter(
+            models.URL.short_code == data.custom_code
+        ).first()
+
+        if existing:
+            raise HTTPException(
+                status_code=400,
+                detail="Custom URL already taken"
+            )
+
+        short_code = data.custom_code
+
+    else:
+        short_code = generate_short_code()
 
     new_url = models.URL(
         original_url=data.url,
@@ -40,6 +65,8 @@ def create_short_url(data: URLCreate, db: Session = Depends(get_db)):
     db.refresh(new_url)
 
     return {"short_url": short_code}
+
+
 
 @app.get("/{code}")
 def redirect_url(code: str, db: Session = Depends(get_db)):
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,7 @@
 # app\schemas.py
 from pydantic import BaseModel
+from typing import Optional
 
 class URLCreate(BaseModel):
     url: str
+    custom_code: Optional[str] = None
