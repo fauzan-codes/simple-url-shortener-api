6870fe43ffba78a9e221b13638e6cb360bc26c9d
Fauzan <azharfauzan472@gmail.com>
2026-04-17 02:12:13 +0700

    update feature UI/UX

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 #  app\main.py
-
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi import Request, FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from fastapi.responses import RedirectResponse
-# from fastapi import HTTPException
+from fastapi.responses import RedirectResponse, HTMLResponse
 
 from app.database import engine, Base, SessionLocal
 from app import models
 from app.schemas import URLCreate
 from app.utils import generate_short_code
 
+from urllib.parse import urlparse
 import re
 
-app = FastAPI()
+app = FastAPI(
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None
+)
+
+templates = Jinja2Templates(directory="app/templates")
 
 Base.metadata.create_all(bind=engine)
 
@@ -23,17 +29,30 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-def root():
-    return {"message": "API jalan!"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
 
 
 
 @app.post("/shorten")
 def create_short_url(data: URLCreate, db: Session = Depends(get_db)):
 
-    if data.custom_code:
+    if not data.url:
+        raise HTTPException(status_code=400, detail="URL cannot be empty")
 
+    parsed = urlparse(data.url)
+    if not parsed.scheme or not parsed.netloc:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid URL format (example: https://google.com)"
+        )
+
+    if data.custom_code:
         if not re.match("^[a-zA-Z0-9_-]+$", data.custom_code):
             raise HTTPException(
                 status_code=400,
